d511c2800faba3e616ba07175de772f4a91c3cec
wes337 <wesmoses@gmail.com>
2021-03-16 18:36:03 +0200

    target blank

--- kofta/src/app/components/Footer.tsx
+++ kofta/src/app/components/Footer.tsx
@@ -12,16 +12,28 @@ export const Footer: React.FC<FooterProps> = ({ isLogin }) => {
 
   return (
     <div className={`flex text-center flex-wrap align-center justify-center`}>
-      <div className={`flex space-x-4 m-auto`}>
+      <div className={`flex flex-grow space-x-4 m-auto`}>
         {isLogin ? (
-          <RegularAnchor href="https://www.youtube.com/watch?v=hy-EhJ_tTQo">
+          <RegularAnchor
+            href="https://www.youtube.com/watch?v=hy-EhJ_tTQo"
+            target="_blank"
+            rel="noreferrer"
+          >
             {t("footer.link_1")}
           </RegularAnchor>
         ) : null}
-        <RegularAnchor href="https://discord.gg/wCbKBZF9cV">
+        <RegularAnchor
+          href="https://discord.gg/wCbKBZF9cV"
+          target="_blank"
+          rel="noreferrer"
+        >
           {t("footer.link_2")}
         </RegularAnchor>
-        <RegularAnchor href="https://github.com/benawad/dogehouse/issues">
+        <RegularAnchor
+          href="https://github.com/benawad/dogehouse/issues"
+          target="_blank"
+          rel="noreferrer"
+        >
           {t("footer.link_3")}
         </RegularAnchor>
         {/* cramps footer on mobile @todo think about how to incorporate this without cramping footer and making the footer really tall */}
@@ -33,7 +45,7 @@ export const Footer: React.FC<FooterProps> = ({ isLogin }) => {
         What's new?
       </RegularAnchor> */}
       </div>
-      <div className={`flex m-auto`}>
+      <div className={`flex flex-shrink m-auto`}>
         <LanguageSelector />
       </div>
     </div>
